52b63518c311cd9a4e948f59a27c3b09bff34398
neony <neonyan15@gmail.com>
2022-05-07 22:49:31 +0300

    add detail views

--- ads/views.py
+++ ads/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import DetailView
 
 from ads.models import Ad, Category
 
@@ -40,3 +41,28 @@ class CategoryView(View):
             })
         return JsonResponse(response, safe=False)
 
+class AdDetailView(DetailView):
+    model = Ad
+    def get(self, request, *args, **kwargs):
+        ad = self.get_object()
+
+        return JsonResponse({
+            "id": ad.id,
+            "name": ad.name,
+            "author": ad.author,
+            "price": ad.price,
+            "description": ad.description,
+            "address": ad.address,
+            "is_published": ad.is_published,
+        })
+
+class CategoryDetailView(DetailView):
+    model = Category
+
+    def get(self, request, *args, **kwargs):
+        category = self.get_object()
+
+        return JsonResponse({
+            'id': category.id,
+            'name': category.name,
+        })
