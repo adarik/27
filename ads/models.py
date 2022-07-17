66e1f10c3440382ee86149fa0fc4a96bca8005fd
neony <neonyan15@gmail.com>
2022-07-17 17:55:04 +0300

    fix field

--- ads/models.py
+++ ads/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Ad(models.Model):
     name = models.CharField(max_length=100)
     author = models.CharField(max_length=50)
-    price = models.IntegerField()
+    price = models.PositiveIntegerField()
     description = models.CharField(max_length=1000, null=True)
     address = models.CharField(max_length=100)
     is_published = models.BooleanField(default=False)
